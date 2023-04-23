7b6143c272faa52c9aa92e2bd7534e36cdc3fa6d
Jason Nguyen <88073140+Kyougi@users.noreply.github.com>
2023-04-23 18:46:02 -0400

    Add files via upload

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,24 +14,73 @@
 
 
 #define MAX_CLIENTS 10
-#define BUFFER 1024
-#define SERVER_PORT "15000"
+#define BUFFER 256
+#define SERVER_PORT "4536"
 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+typedef struct player{
+    char *name;
+    char side;
+    int client_sock;
+} Playing;
 
 typedef struct clients{
     int socket; //client socket
     struct sockaddr_storage address; //their information about their sockets.
     int pthread_position; //what thread are the two players are in
+    int queue_position; 
     int active_game; //1 for currently in an active game and 0 otherwise.
 } Clients;
 
+struct thread_args{
+    int client1_fd;
+    int client2_fd;
+};
+
+void initialize_board(char (table*)[3]){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            table[i][j] = '-';
+        }
+    }
+}
+
+void set_player(int client, ){
+
+}
+
+
+//This is where the tic tac toe game is called. When the game is over, we close the file descriptors of the clients. 
+void * start_game(void *arg){
+    struct thread_args *clients = arg;
+    Playing player_one, player_two;
+    char table[3][3];
+    player_one.name = malloc(sizeof(char) * BUFFER), player_two.name = malloc(sizeof(char) * BUFFER);
+    player_one.client_sock = clients->client1_fd, player_two.client_sock = clients->client2_fd;
+    
+    initialize_board
+}
+
+
 int main(int argc, char * argv[argc + 1]){
     //Variables that we're going to be using...
-    struct sockaddr_storage client_info[MAX_CLIENTS];
+    Clients player[MAX_CLIENTS];
     struct addrinfo host_hints, *result_list; 
-    int server_sockfd; //client_sockfd, clientNum = 0; //one sockfd from server and another from client. clientNum to count the number of clients that has joined
-    socklen_t client_length; //We're going to use this when we call accept.
-    
+    int server_sockfd, client_sockfd, clientNum = 0, acceptable = 0, threads = 0; //one sockfd from server and another from client. clientNum to count the number of clients that has joined
+    pthread_t games[MAX_CLIENTS/2];
+    struct thread_args arg[MAX_CLIENTS/2];
+
+    //Initializing the Clients struct...
+    for(int i = 0; i < MAX_CLIENTS; i++){
+        player[i].socket = 0;
+        player[i].pthread_position = 0;
+        player[i].queue_position = 0;
+        player[i].active_game = 0;
+    }
+
+   
 
     //where we set the process of connections 
     memset(&host_hints, 0, sizeof(struct addrinfo));
@@ -38,7 +88,7 @@ int main(int argc, char * argv[argc + 1]){
     host_hints.ai_socktype = SOCK_STREAM;
     host_hints.ai_flags = AI_PASSIVE;
 
-    int info = getaddrinfo(NULL, SERVER_PORT, &host_hints, &result_list);
+    int info = getaddrinfo("top.cs.rutgers.edu", SERVER_PORT, &host_hints, &result_list);
     if(info < 0){
         fprintf(stderr, "Error in getting address info: %s\n", gai_strerror(info));
         exit(EXIT_FAILURE);
@@ -47,7 +97,7 @@ int main(int argc, char * argv[argc + 1]){
     int error = 0;
     //Setting up a connection
     //List used to only serve what internet connections that works with a specific IP Address. Currently in progress as I gain more knowledge.
-    for(struct addrinfo *list = result_list; forloop_list != NULL; forloop_list = forloop_list->ai_next){
+    for(struct addrinfo *list = result_list; list != NULL; list = list->ai_next){
 
         
         //returns the socket file descirptor for the server 
@@ -56,30 +106,81 @@ int main(int argc, char * argv[argc + 1]){
         } 
         //bind socket to an ip address or port
         if((error = bind(server_sockfd, list->ai_addr, list->ai_addrlen)) < 0){
-            perror("Bind error: ");
             close(server_sockfd);
             continue;
         } 
         //will execute listen if there aren't any errors
         if((error = listen(server_sockfd, MAX_CLIENTS)) < 0 ){
-            perror("Listen error: ");
             close(server_sockfd);
             continue;
         }
 
     }
     //freeing after we're done with the 
-    
+    free(result_list);
+    pthread_mutex_init(&lock, NULL);
+    printf("Listening for incoming connections on port 4536...\n");
 
-    printf("Currently listening for incoming connections...\n");
-
+    //Waiting for a connection...
     while(1){
-        Clients * players = malloc(sizeof(Clients) * MAX_CLIENTS);
-        pthread lobbies[MAX_CLIENTS];
-        host_length = sizeof(struct sockaddr_storage);
-
-        //make sure to use the accept function here. This part of the code is supposed to detect whether or not we got more or equal to 2 connections from the clients. If there are two connections, we should be able to use the select() function and use pthread_create() and pthread_join() in that other. Select() should be able to pick two different file descriptors. If you need help with select(), dm me and I'll be able to search and tell you the information to make this easier. Menendez's lectures are somewhat unreliable at this time. 
+        while(acceptable != 2 || clientNum == MAX_CLIENTS){
+            if((client_sockfd = accept(server_sockfd, (struct sockaddr*)&player[clientNum].address, NULL)) < 0){
+                perror("Accept error: ");
+                continue;
+            } else {
+                //queueing client information while preventing other clients intervening.
+                pthread_mutex_lock(&lock);
+                printf("PLAY|9|Client %d\n", clientNum + 1);
+                player[clientNum].socket = client_sockfd;
+                player[clientNum].queue_position = clientNum;
+                clientNum++;
+                acceptable++;
+                pthread_mutex_unlock(&lock); 
+            }
+            //in the event where acceptable = 2, we have to set them up for the game by checking if they're in the active game. If they aren't, then we collect their file descriptrs 
+            if(acceptable == 2){
+                
+                int sockets[acceptable];
+                int position[acceptable];
+                int count = 0; 
+                for(int i = 0; i < MAX_CLIENTS; i++){
+                    if(count == 2){
+                        break;
+                    }
+                    if(player[i].active_game != 1){
+                        player[i].active_game = 1;
+                        sockets[count] = player[i].socket;
+                        position[count] = i;
+                        count++;
+                    }
+                }
+                arg[threads].client1_fd = sockets[0];
+                arg[threads].client2_fd = sockets[1];
+                int error;
+            
+                if((error = pthread_create(&games[threads], NULL, start_game, (void*)&arg)) < 0){
+                    fprintf(stderr, "Error creating a thread: %s\n", strerror(error));
+                    continue;
+                } else {
+                    player[position[0]].pthread_position = threads;
+                    player[position[1]].pthread_position = threads;
+                }
+                if((error = pthread_join(games[threads], NULL)) < 0){
+                    fprintf(stderr, "Error joining thread: %s\n", strerror(error));
+                    exit(EXIT_FAILURE);
+                }
+                thread++;
+                
+                
+            }
+            acceptable = 0;
+            
+        }
+        
+        
     }
+    puts("Shutting down\n");
+    close(server_sockfd);
 
 
     
